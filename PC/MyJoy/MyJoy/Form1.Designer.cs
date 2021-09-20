8a2904df442ef31464600dfc6566b243524868f8
MSVCM <70491601+MSVCM@users.noreply.github.com>
2021-09-20 18:54:49 +0800

    Add files via upload

---
+++ PC/MyJoy/MyJoy/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿
+namespace MyJoy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ComboBox_Target = new System.Windows.Forms.ComboBox();
+            this.label_linkState = new System.Windows.Forms.Label();
+            this.label_ip = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_port = new System.Windows.Forms.TextBox();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.ExistToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ShowToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ComboBox_Target
+            // 
+            this.ComboBox_Target.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ComboBox_Target.FormattingEnabled = true;
+            this.ComboBox_Target.Location = new System.Drawing.Point(12, 12);
+            this.ComboBox_Target.Name = "ComboBox_Target";
+            this.ComboBox_Target.Size = new System.Drawing.Size(158, 26);
+            this.ComboBox_Target.TabIndex = 1;
+            this.ComboBox_Target.SelectedIndexChanged += new System.EventHandler(this.ComboBox_Target_SelectedIndexChanged);
+            // 
+            // label_linkState
+            // 
+            this.label_linkState.AutoSize = true;
+            this.label_linkState.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.label_linkState.Font = new System.Drawing.Font("宋体", 10F);
+            this.label_linkState.Location = new System.Drawing.Point(12, 52);
+            this.label_linkState.Name = "label_linkState";
+            this.label_linkState.Size = new System.Drawing.Size(159, 20);
+            this.label_linkState.TabIndex = 4;
+            this.label_linkState.Text = "链接状态:未连接";
+            // 
+            // label_ip
+            // 
+            this.label_ip.AutoSize = true;
+            this.label_ip.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.label_ip.Location = new System.Drawing.Point(252, 15);
+            this.label_ip.Name = "label_ip";
+            this.label_ip.Size = new System.Drawing.Size(62, 18);
+            this.label_ip.TabIndex = 5;
+            this.label_ip.Text = "label2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(197, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 18);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "端口：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(215, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 18);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "ip：";
+            // 
+            // textBox_port
+            // 
+            this.textBox_port.Location = new System.Drawing.Point(255, 52);
+            this.textBox_port.Name = "textBox_port";
+            this.textBox_port.Size = new System.Drawing.Size(100, 28);
+            this.textBox_port.TabIndex = 8;
+            this.textBox_port.Text = "40425";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "MyJoy";
+            this.notifyIcon1.Visible = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ExistToolStripMenuItem,
+            this.ShowToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(117, 64);
+            // 
+            // ExistToolStripMenuItem
+            // 
+            this.ExistToolStripMenuItem.Name = "ExistToolStripMenuItem";
+            this.ExistToolStripMenuItem.Size = new System.Drawing.Size(116, 30);
+            this.ExistToolStripMenuItem.Text = "退出";
+            this.ExistToolStripMenuItem.Click += new System.EventHandler(this.ExistToolStripMenuItem_Click);
+            // 
+            // ShowToolStripMenuItem
+            // 
+            this.ShowToolStripMenuItem.Name = "ShowToolStripMenuItem";
+            this.ShowToolStripMenuItem.Size = new System.Drawing.Size(116, 30);
+            this.ShowToolStripMenuItem.Text = "窗口";
+            this.ShowToolStripMenuItem.Click += new System.EventHandler(this.ShowToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(380, 91);
+            this.Controls.Add(this.textBox_port);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label_ip);
+            this.Controls.Add(this.label_linkState);
+            this.Controls.Add(this.ComboBox_Target);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "MyJoy";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ComboBox ComboBox_Target;
+        private System.Windows.Forms.Label label_linkState;
+        private System.Windows.Forms.Label label_ip;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_port;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ExistToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ShowToolStripMenuItem;
+    }
+}
+
---
+++ PC/MyJoy/MyJoy/Form1.cs
@@ -0,0 +1,289 @@
+﻿#define ROBUST
+//#define EFFICIENT
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using vJoyInterfaceWrap;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.IO;
+
+namespace MyJoy
+{
+    public partial class Form1 : Form
+    {
+        public struct User
+        {
+            public string name;
+            public uint ID;
+            public vJoy joystick;
+            public String linkName;
+            public Receive socket;
+            public bool connected;
+        }
+        Dictionary<string, uint> dict = new Dictionary<string, uint>();
+
+        Socket server;
+        List<User> users = new List<User>();
+        int userCount = 0;
+        long maxval;
+
+        List<Receive> clients = new List<Receive>();
+
+        System.Windows.Forms.Label[] labels = new System.Windows.Forms.Label[4];
+
+        public Form1()
+        {
+            InitializeComponent();
+            Control.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Create one joystick object and a position structure.
+            Get_VJD();
+            AddListen(int.Parse(textBox_port.Text), IPAddress.Parse(GetIPAddress()));
+            label_ip.Text = GetIPAddress();
+            try
+            {
+                string s = File.ReadAllText(Application.StartupPath + "/inf.txt");
+                string[] str = s.Split('|');
+                foreach (string st in str)
+                {
+                    string[] strs = st.Split('=');
+                    dict.Add(strs[0], uint.Parse(strs[1]));
+                }
+            }
+            catch (Exception E)
+            {
+                MessageBox.Show(E.Message);
+                System.Environment.Exit(0);
+            }
+            Thread thread = new Thread(CheckLink);
+            thread.Start();
+            this.FormClosing += new FormClosingEventHandler(Form1_FormClosing);
+
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            for (int i = 0; i < clients.Count; i++)
+            {
+                clients[i].client.Shutdown(SocketShutdown.Both);
+                //clients[i].client.Close(1000);
+            }
+            System.Environment.Exit(0);
+        }
+
+        void CheckLink()
+        {
+            while (true)
+            {
+                for (int i = clients.Count - 1; i >= 0; i--)
+                {
+                    try
+                    {
+                        Thread.Sleep(100);
+                        clients[i].time -= 0.1f;
+                        if (clients[i].time < 0)
+                        {
+                            clients[i].receiveThread.Abort();
+                            clients[i].receiveThread.Join();
+                            User u = users[clients[i].num];
+                            u.linkName = "未连接";
+                            u.connected = false;
+                            users[clients[i].num] = u;
+                            if (ComboBox_Target.SelectedIndex == clients[i].num)
+                                label_linkState.Text = "连接状态：" + users[ComboBox_Target.SelectedIndex].linkName;
+                            ComboBox_Target.SelectedIndex = clients[i].num;
+
+                            clients[i] = null;
+                            clients.RemoveAt(i);
+                        }
+                    }
+                    catch
+                    {
+                    }
+                }
+            }
+        }
+
+        private void Get_VJD()
+        {
+            vJoy joy = new vJoy();
+            int k = 1;
+            for (uint i = 1; i <= 16; i++)
+            {
+                if (joy.GetVJDStatus(i) == VjdStat.VJD_STAT_FREE)
+                {
+                    joy.AcquireVJD(i);
+                    joy.ResetAll();
+                    joy.GetVJDAxisMax(i, HID_USAGES.HID_USAGE_X, ref maxval);
+                    User u = new User();
+                    u.name = "User" + k++;
+                    u.ID = i;
+                    u.joystick = joy;
+                    u.linkName = "未连接";
+                    u.connected = false;
+                    u.name = string.Format("vjoy device {0}", i);
+                    users.Add(u);
+                    ComboBox_Target.Items.Add(u.name);
+                    userCount++;
+                }
+            }
+            maxval /= 2;
+            ComboBox_Target.SelectedIndex = 0;
+        }
+
+        private void ComboBox_Target_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            label_linkState.Text = "连接状态:" + users[ComboBox_Target.SelectedIndex].linkName;
+        }
+
+        public string GetIPAddress()
+        {
+            foreach (IPAddress i in Dns.GetHostEntry(Dns.GetHostName()).AddressList)
+            {
+                if (i.AddressFamily == AddressFamily.InterNetwork)
+                    return i.ToString();
+            }
+            return "";
+        }
+
+        void AddListen(int port, IPAddress ip)
+        {
+            server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            server.Bind(new IPEndPoint(ip, port));
+            server.Listen(16);
+            Thread thread = new Thread(ListenClientCount);
+            thread.IsBackground = true;
+            thread.Start();
+        }
+
+        void ListenClientCount()
+        {
+            while (true)
+            {
+                Socket client = server.Accept();
+                client.SetSocketOption(SocketOptionLevel.Tcp, SocketOptionName.NoDelay, true);
+                byte[] buffer = new byte[1024];
+                int recLen = client.Receive(buffer);
+                int i = 0;
+                for (i = 0; i < users.Count; i++)
+                {
+                    if (users[i].connected)
+                        continue;
+                    User u = users[i];
+                    u.linkName = Encoding.UTF8.GetString(buffer, 0, recLen);
+                    u.connected = true;
+                    users[i] = u;
+                    client.Send(Encoding.UTF8.GetBytes("SUCCEED|" + u.name));
+
+                    if (ComboBox_Target.SelectedIndex == i)
+                        label_linkState.Text = "连接状态：" + users[ComboBox_Target.SelectedIndex].linkName;
+                    ComboBox_Target.SelectedIndex = i;
+
+                    Thread thread = new Thread(ReceiveClient);
+                    Receive r = new Receive(client, i, thread);
+                    clients.Add(r);
+                    thread.Start(r);
+                    break;
+                }
+                client.Send(Encoding.UTF8.GetBytes("FAIL|连接已满"));
+            }
+
+            void ReceiveClient(Object o)
+            {
+                while (true)
+                {
+                    try
+                    {
+                        Receive rec = o as Receive;
+                        byte[] buffer = new byte[1024];
+                        int recLen = rec.client.Receive(buffer);
+                        rec.result = Encoding.UTF8.GetString(buffer, 0, recLen);
+                        foreach (string s in rec.result.Split('+'))
+                        {
+                            if (s == "0")
+                                rec.time = 6;
+                           else if (s != "")
+                                SetOption(s, rec.num);
+                        }
+                    }
+                    catch
+                    {
+
+                    }
+                }
+            }
+
+            void SetOption(string re, int i)
+            {
+                User u = users[i];
+                string[] a = re.Split('|');
+                try
+                {
+                    switch (a[0])
+                    {
+                        case "JOYSTICK":
+                            HID_USAGES h1 = HID_USAGES.HID_USAGE_X
+                                , h2 = HID_USAGES.HID_USAGE_Y;
+                            int x = (int)(float.Parse(a[2]) * maxval + maxval), y = (int)(-float.Parse(a[3]) * maxval + maxval);
+                            if (a[1] == "RIGHT")
+                            {
+                                h1 = HID_USAGES.HID_USAGE_RX;
+                                h2 = HID_USAGES.HID_USAGE_RY;
+                            }
+                            u.joystick.SetAxis(x, u.ID, h1);
+                            u.joystick.SetAxis(y, u.ID, h2);
+                            break;
+
+                        case "POV":
+                            //0左1右2上3下
+                            u.joystick.SetDiscPov(int.Parse(a[1]), u.ID, 1);
+                            break;
+                        case "ABXY":
+                            u.joystick.SetBtn(a[2] == "False" ? false : true, u.ID, dict[a[1]]);
+                            u.joystick.SetBtn(a[4] == "False" ? false : true, u.ID, dict[a[3]]);
+                            u.joystick.SetBtn(a[6] == "False" ? false : true, u.ID, dict[a[5]]);
+                            u.joystick.SetBtn(a[8] == "False" ? false : true, u.ID, dict[a[7]]);
+                            break;
+                        case "Trigger":
+                            HID_USAGES h3 = HID_USAGES.HID_USAGE_RZ;
+                            if (a[1] == "L")
+                                h3 = HID_USAGES.HID_USAGE_Z;
+                            u.joystick.SetAxis((int)(float.Parse(a[2]) * maxval + maxval), u.ID, h3);
+                            break;
+                        case "OTHER":
+                            u.joystick.SetBtn(a[2] == "False" ? false : true, u.ID, dict[a[1]]);
+                            break;
+                    }
+                }
+                catch
+                {
+
+                }
+            }
+        }
+
+        private void ExistToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < clients.Count; i++)
+            {
+                clients[i].client.Shutdown(SocketShutdown.Both);
+                //clients[i].client.Close(1000);
+            }
+            System.Environment.Exit(0);
+        }
+
+        private void ShowToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ShowInTaskbar = !ShowInTaskbar;
+            Visible = ShowInTaskbar;
+        }
+    }
+}
